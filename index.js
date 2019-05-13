1c9053f55d198edd38a8a81f6f1cdd113684ebcc
Jinliang <hou80houzhu@vip.qq.com>
2019-05-13 21:13:10 +0800

    pass

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Server = require("ada-cloud-boot");
 const router = require("./src/router");
+const { Boost, Service } = require("ada-cloud-util/sql");
+const Path = require("path");
 
 let server = new Server();
 server.use(router.routes());
+server.on('configchange', () => {
+    let { db } = server.config;
+    Boost.update(db);
+});
 server.on('started', () => {
 });
-server.startup();
+server.startup(({ context, config }) => {
+    context.Service = Service;
+    return Boost.scan(Path.relative(__dirname, "./src"), config.db);
+});
