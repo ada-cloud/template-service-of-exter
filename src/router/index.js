5868f82ad0f04feb24a76481ef1380a3527d4089
Jinliang <hou80houzhu@vip.qq.com>
2019-05-13 20:42:24 +0800

    pass

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 const Router = require("koa-router");
+const Result=require("ada-cloud-util/result");
 const router = new Router();
-router.get('/get', ({ response }) => {
-    response.body = 'template-service-name';
+
+router.get('/get', ({ Service, response }) => {
+    return Service.getService("testService").test().then(() => {
+        response.body = 'template-service-name';
+    }).catch(() => {
+        response.body = Result.getErrorResult().getResponseData();
+    });
 });
 
 module.exports = router;
