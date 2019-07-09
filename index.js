01dff76190425ed22804c9ee65977220963e5cf6
hou80houzhu <topolr@vip.qq.com>
2019-07-09 20:47:54 +0800

    pass

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const Path = require("path");
 const Server = require("./src/index");
-const { Boost, Service } = require("ada-cloud-util/boost");
+const { Boost } = require("ada-cloud-util/boost");
+const bodyParser = require('koa-bodyparser');
 
 let server = new Server();
+server.use(bodyParser());
 server.on('configchange', () => {
     let { db } = server.config;
     Boost.updateDatabase(db);
